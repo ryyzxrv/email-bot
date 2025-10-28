729dc7ea10ef81ee61404a7b8410bd42e3ebbb11
r4yy. <argaf.nugraha@gmail.com>
2025-10-28 09:43:37 +0300

    Update config.js

--- node_wa/config.js
+++ node_wa/config.js
@@ -1,5 +1,11 @@
+export const telegramBotToken = "8266869214:AAFhzKVEaBRhIVxVKDZlwrS7u375bci_vqs"; // token bot Telegram
+export const ownerId = 7562165596; // ganti dengan user_id Telegram kamu
+export const message = {
+  waNotConnected: "⚠️ WhatsApp belum tersambung. Scan QR dulu di terminal.",
+};
+
 export default {
-  sessionName: './session',
-  hasilFolder: './hasil',
-  telegramBotUrl: 'http://localhost:5000/wa-result', // nanti kita kirim hasil ke bot.py
+  sessionName: "./session",
+  hasilFolder: "./hasil",
+  telegramBotUrl: "http://localhost:5000/wa-result",
 };
